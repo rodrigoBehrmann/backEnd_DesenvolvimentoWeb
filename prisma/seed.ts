67e36f1fe57f8580c6cdc3569960863a5b9f5585
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2023-05-25 21:09:44 -0300

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,37 +2,62 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main(){
-    await prisma.usuario.create({
-        data: {
-            "nome": "Admin",
-            "matricula": "202233094",
-            "cpf": "0000000000",                   
-            "alunoId": "001",                   
-            "login": "admin",
-            "senha": "admin"
-        }
-    })
+async function getCursoId(): Promise<string | null> {
+  const curso = await prisma.curso.findFirst({
+    where: {
+      codigo: '001',
+    },
+    select: {
+      id: true,
+    },
+  });
 
-    await prisma.curso.create({
-        data: {
-            "nome": "Admin",
-            "codigo": "001"
-        }
-    })
-
-    await prisma.disciplina.create({
-        data: {
-            "nome": "Admin",
-            "carga_horaria": "60",
-            "disciplinaId": "001"
-        }
-    })
+  return curso.id;
 }
 
-main().catch(e => {
+async function CriaCurso() {
+  await prisma.curso.create({
+    data: {
+      nome: 'Computacao',
+      codigo: '001',
+    },
+  });
+}
+
+async function CriaDisciplina(codigo: string) {
+  await prisma.disciplina.create({
+    data: {
+      nome: 'Admin',
+      carga_horaria: '60',
+      disciplinaId: codigo,
+    },
+  });
+}
+async function CriaUsuario(codigo: string) {
+  await prisma.usuario.create({
+    data: {
+      nome: 'Admin',
+      matricula: '202233094',
+      cpf: '0000000000',
+      alunoId: codigo,
+      login: 'admin',
+      senha: 'admin',
+    },
+  });
+}
+
+async function main() {
+  await CriaCurso();
+  const codigo = await getCursoId();
+  await CriaDisciplina(codigo);
+  await CriaUsuario(codigo);
+}
+
+main()
+  .catch((e) => {
     console.log(e);
     process.exit(1);
-}).finally(() =>{
+  })
+  .finally(() => {
     prisma.$disconnect();
-})
+  });
